78fe7c1509075cae515d968ecdabc721c6d8fec4
Riya Gupta <160048744+Riya3011@users.noreply.github.com>
2024-02-25 20:01:54 +0530

    Create feespaid.cpp

---
+++ feespaid.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+int i;
+cout<<"Enter the amount paid";
+cin>>i;
+if (i<350000){
+  cout<<"fees has not been paid "<<endl;
+ } else{
+    cout<<"fees is paid"<<endl;
+  }
+ return 0;
+}
