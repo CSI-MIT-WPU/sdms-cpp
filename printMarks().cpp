ac884830c4d773a229608116d2151638bfd5103b
Shambhavee1105 <160020341+Shambhavee1105@users.noreply.github.com>
2024-02-28 20:36:29 +0530

    Create printMarks().cpp

---
+++ printMarks().cpp
@@ -0,0 +1,11 @@
+PRINT MARKS:
+
+void printmarks(float intpps, float extpps, float intdmgt, float extdmgt, float intbeee, float extbeee)
+{
+cout<<"The internal marks for PPS are :"<<intpps;
+cout<<"The external marks for PPS are :"<<extpps;
+cout<<"The internal marks for DMGT are :"<<intdmgt;
+cout<<"The external marks for DMGT are :"<<extdmgt;
+cout<<"The internal marks for BEEE are :"<<intbeee;
+cout<<"The external marks for BEEE are :"<<extbeee;
+}
