724deaa658e693f010f841d2bcb1718d97423b84
vaidehitiwari <104577611+vaidehitiwari@users.noreply.github.com>
2024-02-28 19:20:16 +0530

    Update CheckForScholarship.cpp

--- CheckForScholarship.cpp
+++ CheckForScholarship.cpp
@@ -1,35 +1,58 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
-class CheckForScholarship {
+class Student {
 private:
     float gpa;
     float jeemain;
     float cet;
+    string branch;
 
 public:
-    CheckForScholarship(float student_gpa, float student_jeemain, float student_cet)
-        : gpa(student_gpa), jeemain(student_jeemain), cet(student_cet) {}
+   
+    Student(float student_gpa, float student_jeemain, float student_cet, const string& student_branch)
+        : gpa(student_gpa), jeemain(student_jeemain), cet(student_cet), branch(student_branch) {}
 
-    bool isEligibleForScholarship(string branch) const {
-        if (branch == "B.TECH CS") {
-            return (gpa >= 8.0 && (jeemain >= 96.0 || cet >= 98.0));
-        } else if (branch == "B.TECH ENTC") {
-            return (gpa >= 8.0 && (jeemain >= 95.0 || cet >= 96.0));
-        } else if (branch == "B.TECH CIVIL" || branch == "B.TECH MECH") {
-            return (gpa >= 8.0 && (jeemain >= 95.0 || cet >= 96.0));
-        } else if (branch == "DIPLOMASTUD") {
-            return (gpa >= 7.0 && (jeemain >= 90.0 || cet >= 90.0));
-        } else {
-            return false;
+    string getBranch() const {
+        return branch;
+    }
+
+    float getGPA() const {
+        return gpa;
+    }
+
+    float getJEEMainScore() const {
+        return jeemain;
+    }
+
+    float getCETScore() const {
+        return cet;
+    }
+};
+
+class ScholarshipChecker {
+public:
+    bool isEligibleForScholarship(const Student& student) const {
+        const string& branch = student.getBranch();
+        float gpa = student.getGPA();
+        float jeemain = student.getJEEMainScore();
+        float cet = student.getCETScore();
+
+        if ((branch == "B.TECH CS" && gpa >= 8.0 && (jeemain >= 96.0 || cet >= 98.0)) ||
+            ((branch == "B.TECH ENTC" || branch == "B.TECH CIVIL" || branch == "B.TECH MECH") && gpa >= 8.0 && (jeemain >= 95.0 || cet >= 96.0)) ||
+            (branch == "DIPLOMASTUD" && gpa >= 7.0 && (jeemain >= 90.0 || cet >= 90.0))) {
+            return true;
         }
+
+        return false;
     }
 };
 
 int main() {
     float student_gpa, student_jeemain, student_cet;
-    
+
     cout << "Enter student's branch (e.g., B.TECH CS, B.TECH ENTC, B.TECH CIVIL, B.TECH MECH): ";
     string student_branch;
     getline(cin, student_branch);
@@ -43,13 +66,14 @@ int main() {
     cout << "STATECET score: ";
     cin >> student_cet;
 
+    Student student(student_gpa, student_jeemain, student_cet, student_branch);
 
-    CheckForScholarship student(student_gpa, student_jeemain, student_cet);
+    ScholarshipChecker scholarshipChecker;
 
-    if (student.isEligibleForScholarship(student_branch)) {
-        cout << "Student is eligible for a scholarship in the branch " << student_branch << ".\n";
+    if (scholarshipChecker.isEligibleForScholarship(student)) {
+        cout << "Student is eligible for a scholarship in the branch " << student.getBranch() << ".\n";
     } else {
-        cout << "Student does not meet the eligibility criteria for a scholarship in the branch " << student_branch << ".\n";
+        cout << "Student does not meet the eligibility criteria for a scholarship in the branch " << student.getBranch() <<"./n";
     }
 
     return 0;
