e4eaa47d9140add56ad555abc5bc0cc339be160d
Arnav-Nikumbh <159717898+Arnav-Nikumbh@users.noreply.github.com>
2024-02-28 19:34:45 +0530

    Update searchstud.cpp

--- searchstud.cpp
+++ searchstud.cpp
@@ -2,10 +2,16 @@ void searchstud(long int prn)
 {
     for(i=0, i<n; i++)
     {
-        if(prn == student.prn_no[i])
+        if(prn == studentObj.prn_no[i])
         {
             cout<<"Prn found"<<endl;
+            cout << "\nPRN: " <<    studentObj.prn_no[i]
+         << "\nName: " << studentObj.name
+         << "\nRoll Number: " << studentObj.roll_no[i]
+         << "\nDate of Birth: " << studentObj.dateOfBirth[i]
+         << "\nGender: " << studentObj.gender[i] << endl;
         }
+
     }
     
 }
