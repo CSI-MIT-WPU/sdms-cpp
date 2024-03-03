4f41d65837a6a5d232cae6bc194e5f9493f58f6a
Riya Gupta <160048744+Riya3011@users.noreply.github.com>
2024-03-03 19:56:11 +0530

    Update Assign course.cpp

--- Assign course.cpp
+++ Assign course.cpp
@@ -1,49 +1,43 @@
 #include <iostream>
+#include <string>
 using namespace std;
-int main() {
-string courses[] = {"Discrete Mathematics with Graph Theory ", "fcasd", 
-    "Programming and Problem Solving", "Chemistry ", "Engineering Mechanics ",
-     "Linear Algebra and Differential Calculus ", "Physics","Biology for Engineers",
-     "Engineering Graphics"};
-    int numCourses = 9;
-int choice;
- cout << "Enter your id: ";
-cin>>choice;
-switch(choice){
-    case 1:
-    cout<<"The course assigned to you is\n"<<courses[0]<<endl;
-break;
-case 2:
-cout<<"the course assigned to you is\n"<<courses[1]<<endl;
-break;
-case 3:
-cout<<"the course assigned to you is\n"<<courses[2]<<endl;
-break;
-case 4:
-cout<<"the course assigned to you is\n"<<courses[3]<<endl;
-break;
-case 5:
-cout<<"the course assigned to you is\n"<<courses[4]<<endl;
-break;
-case 6:
-cout<<"the course assigned to you is\n"<<courses[5]<<endl;
-break;
-case 7:
-cout<<"the course assigned to you is\n"<<courses[6]<<endl;
-break;
-case 8:
-cout<<"the course assigned to you is\n"<<courses[7]<<endl;
-break;
-case 9:
-cout<<"the course assigned to you is\n"<<courses[8]<<endl;
-break;
-case 10:
-cout<<"the course assigned to you is\n"<<courses[9]<<endl;
-break;
 
-default:
-cout << "Invalid id. Please enter a valid id.\n";
-break;
+class CourseAssigned {
+private:
+    static const int numCourses = 9;
+    string courses[numCourses];
+
+public:
+    CourseAssigned() {
+        courses[0] = "Discrete Mathematics with Graph Theory";
+        courses[1] = "fcasd";
+        courses[2] = "Programming and Problem Solving";
+        courses[3] = "Chemistry";
+        courses[4] = "Engineering Mechanics";
+        courses[5] = "Linear Algebra and Differential Calculus";
+        courses[6] = "Physics";
+        courses[7] = "Biology for Engineers";
+        courses[8] = "Engineering Graphics";
+    }
+
+    void assignCourse(int id) const {
+        if (id >= 1 && id <= numCourses) {
+            cout << "The course assigned to you is\n" << courses[id - 1] << std::endl;
+        } else {
+            cout << "Invalid id. Please enter a valid id between 1-9\n";
+        }
+    }
+};
+
+int main() {
+    CourseAssigned assigned;
+
+    int choice;
+    cout << "Enter your id(1-9): ";
+    cin >> choice;
+
+    assigned.assignCourse(choice);
+
+    return 0;
 }
-return 0;
-}
+
